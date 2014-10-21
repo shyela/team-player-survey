7a01b570156ce61d64f9451142dac65e711f0c37
Shy Aberman <shy@aberman.com>
2014-10-20 22:44:15 -0700

    misc

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -aP --delete --exclude=conf --exclude=.* --exclude=deploy.sh --exclude=test . $LOGNAME@expweb01.staging.vlt.smilebox.com:/var/www/vlt-team/public_html/survey/
+rsync -aP --delete --exclude=conf --exclude=.* --exclude=deploy.sh --exclude=test . $LOGNAME@expweb01.prod.vlt.smilebox.com:/var/www/vlt-team/public_html/survey/
